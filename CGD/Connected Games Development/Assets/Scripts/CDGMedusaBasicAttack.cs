4b0867843557cabee07a5e705b384f5742790b9c
Sam <samuelmcdouall@gmail.com>
2022-03-17 13:50:11 +0000

    Update CDGMedusaBasicAttack.cs
    Updated for PUN

--- CGD/Connected Games Development/Assets/Scripts/CDGMedusaBasicAttack.cs
+++ CGD/Connected Games Development/Assets/Scripts/CDGMedusaBasicAttack.cs
@@ -1,3 +1,5 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +9,15 @@ public class CDGMedusaBasicAttack : MonoBehaviour
     public GameObject OwnPlayer;
     Collider _repelCollider;
     [SerializeField]
-    float RepelBubbleDuration;
+    float _repelBubbleDuration;
     float _repelBubbleDurationTimer;
     [SerializeField]
-    float RepelForce;
+    float _repelForce;
     [SerializeField]
-    float RepelCooldown;
+    float _repelCooldown;
     float _repelCooldownTimer;
     public bool _readyToRepel;
+    Rigidbody _tempRb;
 
 
     void Start()
@@ -30,7 +33,7 @@ public class CDGMedusaBasicAttack : MonoBehaviour
     {
         if (_repelCollider.enabled)
         {
-            if (_repelBubbleDurationTimer > RepelBubbleDuration)
+            if (_repelBubbleDurationTimer > _repelBubbleDuration)
             {
                 _repelBubbleDurationTimer = 0.0f;
                 _repelCollider.enabled = false;
@@ -44,7 +47,7 @@ public class CDGMedusaBasicAttack : MonoBehaviour
         {
             if (!_readyToRepel)
             {
-                if (_repelCooldownTimer > RepelCooldown)
+                if (_repelCooldownTimer > _repelCooldown)
                 {
                     _repelCooldownTimer = 0.0f;
                     _readyToRepel = true;
@@ -57,8 +60,13 @@ public class CDGMedusaBasicAttack : MonoBehaviour
         }
         if (Input.GetMouseButtonDown(0) && _readyToRepel)
         {
+            print("clicked");
             _repelCollider.enabled = true;
             _readyToRepel = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //ChangeColor(new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f)));
         }
     }
 
@@ -67,9 +75,17 @@ public class CDGMedusaBasicAttack : MonoBehaviour
         if (collider.tag == "Player" && collider.gameObject != OwnPlayer)
         {
             print("hit other player");
-            Vector3 PlayerToEnemyDirection = Vector3.Normalize(collider.gameObject.transform.position - OwnPlayer.transform.position);
-            collider.gameObject.GetComponent<Rigidbody>().AddForce(PlayerToEnemyDirection * RepelForce);
+            Vector3 playerToEnemyDirection = Vector3.Normalize(collider.gameObject.transform.position - OwnPlayer.transform.position);
+            Vector3 forceToAdd = playerToEnemyDirection * _repelForce;
+            int photonViewID = collider.gameObject.GetComponent<PhotonView>().ViewID;
+            OwnPlayer.GetComponent<CGDMedusaPlayer>().KnockbackOtherPlayer(forceToAdd, photonViewID);
         }
     }
+
+
+
+
+
+
     // maybe move actual physics calculation into fixedupdate
 }
