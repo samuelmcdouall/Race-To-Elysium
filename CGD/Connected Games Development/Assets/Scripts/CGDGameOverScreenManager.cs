69f3e837e0fc9ad508626d3b5f2f3f853bd36b55
Sam <samuelmcdouall@gmail.com>
2022-03-18 01:53:41 +0000

    GameOverScreenManager scripts added

---
+++ CGD/Connected Games Development/Assets/Scripts/CGDGameOverScreenManager.cs
@@ -0,0 +1,49 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CGDGameOverScreenManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameObject WonScreen;
+    public static GameObject LossScreen;
+    public static bool GameOver;
+
+    void Start()
+    {
+        GameOver = false;
+        WonScreen = GameObject.FindGameObjectWithTag("WinScreen");
+        WonScreen.SetActive(false);
+        LossScreen = GameObject.FindGameObjectWithTag("LossScreen");
+        LossScreen.SetActive(false);
+    }
+    public static void DisplayWinScreen()
+    {
+        print("DISPLAY WIN SCREEN");
+        GameOver = true;
+        Cursor.visible = true;
+        WonScreen.SetActive(true);
+    }
+    public static void DisplayLossScreen()
+    {
+        print("DISPLAY LOSS SCREEN");
+        GameOver = true;
+        Cursor.visible = true;
+        LossScreen.SetActive(true);
+    }
+
+    public void OnClickMainMenuButton()
+    {
+        StartCoroutine(LeaveRoom());
+    }
+    IEnumerator LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom(true);
+        while (PhotonNetwork.InRoom)
+        {
+            yield return null;
+        }
+        PhotonNetwork.LoadLevel("MainMenuScene");
+    }
+}
