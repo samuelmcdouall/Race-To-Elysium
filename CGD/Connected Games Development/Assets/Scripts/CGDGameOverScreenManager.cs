4ae4015efb89bb3e6dab37f120900da3c9c09a0d
Sam <samuelmcdouall@gmail.com>
2022-03-19 18:23:04 +0000

    using puncallbacks

--- CGD/Connected Games Development/Assets/Scripts/CGDGameOverScreenManager.cs
+++ CGD/Connected Games Development/Assets/Scripts/CGDGameOverScreenManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CGDGameOverScreenManager : MonoBehaviour
+public class CGDGameOverScreenManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     public static GameObject WonScreen;
