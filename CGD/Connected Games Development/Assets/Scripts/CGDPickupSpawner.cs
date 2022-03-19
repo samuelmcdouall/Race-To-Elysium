0efd7d9d1c27783069a9845146e2c1228a9efe01
Sam <samuelmcdouall@gmail.com>
2022-03-19 02:33:19 +0000

    Changing the way pickup spawns
    just doing it once for now, since d/cing causes issues with spawning in further pickups

--- CGD/Connected Games Development/Assets/Scripts/CGDPickupSpawner.cs
+++ CGD/Connected Games Development/Assets/Scripts/CGDPickupSpawner.cs
@@ -28,39 +28,43 @@ public class CGDPickupSpawner : MonoBehaviour
         //{
         //    Destroy(gameObject);
         //}
-        _spawnDelayTimer = 0.0f;
-        _spawnDelay = Random.Range(_minSpawnDelay, _maxSpawnDelay);
-        print("initial spawn delay for platform: " + _spawnDelay);
-        SpawnedPickup = false;
+        //_spawnDelayTimer = 0.0f;
+        //_spawnDelay = Random.Range(_minSpawnDelay, _maxSpawnDelay);
+        //print("initial spawn delay for platform: " + _spawnDelay);
+        //SpawnedPickup = false;
+        float random_x_pos = Random.Range(_minXSpawnDis, _maxXSpawnDis);
+        float random_z_pos = Random.Range(_minZSpawnDis, _maxZSpawnDis);
+        Vector3 spawn_position = new Vector3(random_x_pos, 1.0f, random_z_pos) + gameObject.transform.position;
+        GameObject pickup = PhotonNetwork.InstantiateRoomObject(Pickup.name, spawn_position, Quaternion.identity);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //print(_spawnDelay);
-        if (PhotonNetwork.IsMasterClient) // still issue here with platform reference
-        {
-            if (!SpawnedPickup)
-            {
-                if (_spawnDelayTimer > _spawnDelay)
-                {
-                    float random_x_pos = Random.Range(_minXSpawnDis, _maxXSpawnDis);
-                    float random_z_pos = Random.Range(_minZSpawnDis, _maxZSpawnDis);
-                    Vector3 spawn_position = new Vector3(random_x_pos, 1.0f, random_z_pos) + gameObject.transform.position;
-                    GameObject pickup = PhotonNetwork.InstantiateRoomObject(Pickup.name, spawn_position, Quaternion.identity);
-                    pickup.GetComponent<CGDUltimatePickupReduce>().PickupPlatformSpawner = gameObject;
-                    pickup.transform.Find("IncreaseCollider").GetComponent<CGDUltimatePickupIncrease>().PickupPlatformSpawner = gameObject;
-                    SpawnedPickup = true;
-                    _spawnDelayTimer = 0.0f;
-                    _spawnDelay = Random.Range(_minSpawnDelay, _maxSpawnDelay);
-                    print("new spawn delay for platform: " + _spawnDelay);
-                }
-                else
-                {
-                    _spawnDelayTimer += Time.deltaTime;
-                }
-            }
-        }
+        ////print(_spawnDelay);
+        //if (PhotonNetwork.IsMasterClient) // still issue here with platform reference when a player leaves
+        //{
+        //    if (!SpawnedPickup)
+        //    {
+        //        if (_spawnDelayTimer > _spawnDelay)
+        //        {
+        //            float random_x_pos = Random.Range(_minXSpawnDis, _maxXSpawnDis);
+        //            float random_z_pos = Random.Range(_minZSpawnDis, _maxZSpawnDis);
+        //            Vector3 spawn_position = new Vector3(random_x_pos, 1.0f, random_z_pos) + gameObject.transform.position;
+        //            GameObject pickup = PhotonNetwork.InstantiateRoomObject(Pickup.name, spawn_position, Quaternion.identity);
+        //            pickup.GetComponent<CGDUltimatePickupReduce>().PickupPlatformSpawner = gameObject;
+        //            pickup.transform.Find("IncreaseCollider").GetComponent<CGDUltimatePickupIncrease>().PickupPlatformSpawner = gameObject;
+        //            SpawnedPickup = true;
+        //            _spawnDelayTimer = 0.0f;
+        //            _spawnDelay = Random.Range(_minSpawnDelay, _maxSpawnDelay);
+        //            print("new spawn delay for platform: " + _spawnDelay);
+        //        }
+        //        else
+        //        {
+        //            _spawnDelayTimer += Time.deltaTime;
+        //        }
+        //    }
+        //}
     }
 }
