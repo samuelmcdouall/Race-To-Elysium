d3c6c4923da82068fe5236ef370c02475fd883e1
Sam <samuelmcdouall@gmail.com>
2022-03-19 03:44:06 +0000

    Auto load into game scene if there are enough players

--- CGD/Connected Games Development/Assets/Scripts/CGDPlayerSpawner.cs
+++ CGD/Connected Games Development/Assets/Scripts/CGDPlayerSpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Photon.Pun;
 
 public class CGDPlayerSpawner : MonoBehaviour
@@ -16,12 +17,27 @@ public class CGDPlayerSpawner : MonoBehaviour
     float _minSpawnZ;
     [SerializeField]
     float _maxSpawnZ;
+    int _maxPlayers = 2;
+    GameObject _gameSceneLoader;
 
     // Start is called before the first frame update
     void Start()
     {
+        _gameSceneLoader = GameObject.FindGameObjectWithTag("GameSceneLoader");
         Vector2 randomPosition = new Vector3(Random.Range(_minSpawnX, _maxSpawnX), PlayerPrefab.transform.position.y, Random.Range(_minSpawnZ, _maxSpawnZ));
         PhotonNetwork.Instantiate(PlayerPrefab.name, randomPosition, Quaternion.identity);
         // check how many players there are in the scene here, if equal to 4 then set to tru on game scene loader
+        if (SceneManager.GetActiveScene().name == "PlayerLobbyScene")
+        {
+            //GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+            print("There are now: " + playerCount + " players in the lobby");
+            if (playerCount == _maxPlayers)
+            {
+                print("Enough players (" + _maxPlayers + ") to start the game");
+                _gameSceneLoader.GetComponent<CGDGameSceneLoader>().BeginCountDownForAllPlayers();
+
+            }
+        }
     }
 }
