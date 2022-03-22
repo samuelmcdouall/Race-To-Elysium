94dfb7d8aeadb69ec04e78dd1b05d5094b4e3022
Sam <samuelmcdouall@gmail.com>
2022-03-22 15:35:31 +0000

    Need to make sure to disable settings menu if up when Game Over

--- CGD/Connected Games Development/Assets/Scripts/CGDGameOverScreenManager.cs
+++ CGD/Connected Games Development/Assets/Scripts/CGDGameOverScreenManager.cs
@@ -6,37 +6,40 @@ using UnityEngine;
 public class CGDGameOverScreenManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
-    public static GameObject WonScreen;
+    public static GameObject WinScreen;
     public static GameObject LossScreen;
     public static GameObject PauseMenu;
+    public static GameObject SettingsMenu;
     public static bool GameOver;
     public CGDPauseManager PauseManager;
 
     void Start()
     {
         GameOver = false;
-        WonScreen = GameObject.FindGameObjectWithTag("WinScreen");
-        WonScreen.SetActive(false);
+        WinScreen = GameObject.FindGameObjectWithTag("WinScreen");
+        WinScreen.SetActive(false);
         LossScreen = GameObject.FindGameObjectWithTag("LossScreen");
         LossScreen.SetActive(false);
         PauseMenu = GameObject.FindGameObjectWithTag("PauseMenu");
         PauseMenu.SetActive(false);
+        SettingsMenu = GameObject.FindGameObjectWithTag("SettingsMenu");
+        SettingsMenu.SetActive(false);
     }
     public static void DisplayWinScreen()
     {
         print("DISPLAY WIN SCREEN");
-        GameOver = true;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
-        if (PauseMenu.activeSelf)
-        {
-            PauseMenu.SetActive(false);
-        }
-        WonScreen.SetActive(true);
+        TriggerGameOverState();
+        WinScreen.SetActive(true);
     }
     public static void DisplayLossScreen()
     {
         print("DISPLAY LOSS SCREEN");
+        TriggerGameOverState();
+        LossScreen.SetActive(true);
+    }
+
+    private static void TriggerGameOverState()
+    {
         GameOver = true;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
@@ -44,7 +47,10 @@ public class CGDGameOverScreenManager : MonoBehaviourPunCallbacks
         {
             PauseMenu.SetActive(false);
         }
-        LossScreen.SetActive(true);
+        if (SettingsMenu.activeSelf)
+        {
+            SettingsMenu.SetActive(false);
+        }
     }
 
     public void OnClickMainMenuButton()
