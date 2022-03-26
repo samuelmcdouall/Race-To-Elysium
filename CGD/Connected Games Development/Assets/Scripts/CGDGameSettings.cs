3873835e09e346a7982c6d3df7946f377285cce4
Sam <samuelmcdouall@gmail.com>
2022-03-26 02:44:18 +0000

    debug way to change between characters

--- CGD/Connected Games Development/Assets/Scripts/CGDGameSettings.cs
+++ CGD/Connected Games Development/Assets/Scripts/CGDGameSettings.cs
@@ -8,6 +8,7 @@ public class CGDGameSettings : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     public static CGDGameSettings Instance;
+    public static int CharacterNum = 1;
     public static int PlayerNum = -1; // have to decrease this by one if someone in the queue moves up
     public static float MouseSensitivity = -1.0f;
     public static float MusicVolume = -1.0f;
@@ -25,6 +26,23 @@ public class CGDGameSettings : MonoBehaviourPunCallbacks
         MouseSensitivity = PlayerPrefs.GetFloat("Sensitivity", 10.5f);
         MusicVolume = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
         //_view = GetComponent<PhotonView>();
+    }
+
+    //todo debug only
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            CharacterNum = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            CharacterNum = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            CharacterNum = 3;
+        }
     }
 
     //public override void OnLeftRoom()
