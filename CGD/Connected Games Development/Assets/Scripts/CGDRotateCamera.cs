bd3207932248d120883bf317edf2da6706aa7ed7
Sam <samuelmcdouall@gmail.com>
2022-03-31 23:57:43 +0100

    Camera now moves towards the player if clipped

--- CGD/Connected Games Development/Assets/Scripts/CGDRotateCamera.cs
+++ CGD/Connected Games Development/Assets/Scripts/CGDRotateCamera.cs
@@ -19,11 +19,18 @@ public class CGDRotateCamera : MonoBehaviour
     float MouseYMaxClamp = 60.0f;
 
     public PhotonView View;
+    public GameObject OwnPlayer;
+
+    float _maxPossibleCameraDistance;
 
     //[Header("Menus")]
     //public GameObject pause_menu;
     //public GameObject options_menu;
     //public GameObject controls_menu;
+    void Start()
+    {
+        _maxPossibleCameraDistance = Vector3.Distance(transform.position, CameraTargetToRotateAround.position); 
+    }
     void Update()
     {
         //if (!pause_menu.activeSelf && !options_menu.activeSelf && !controls_menu.activeSelf)
@@ -32,6 +39,26 @@ public class CGDRotateCamera : MonoBehaviour
         {
             GetMouseInput();
             CameraTargetToRotateAround.rotation = Quaternion.Euler(_mouseY, _mouseX, 0.0f);
+        }
+        RaycastHit hit;
+        Vector3 cameraToPlayerDirection = OwnPlayer.transform.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        cameraToPlayerDirection = cameraToPlayerDirection.normalized;
+        Vector3 maxPossibleCameraPosition = OwnPlayer.transform.position + -cameraToPlayerDirection * _maxPossibleCameraDistance;
+        Debug.DrawRay(maxPossibleCameraPosition, cameraToPlayerDirection * 10.0f, Color.cyan, 1.0f);
+        
+        if (Physics.Raycast(maxPossibleCameraPosition, cameraToPlayerDirection, out hit, 10.0f))
+        {
+            if (hit.transform.gameObject != OwnPlayer)
+            {
+                print("can't see player, move camera closer");
+                float playerToObstacleDistance = Vector3.Distance(hit.point, OwnPlayer.transform.position);
+                transform.position = OwnPlayer.transform.position + -cameraToPlayerDirection * (playerToObstacleDistance);
+            }
+            else
+            {
+                print("can see player, move camera to normal position");
+                transform.position = maxPossibleCameraPosition;
+            }
         }
         //}
     }
