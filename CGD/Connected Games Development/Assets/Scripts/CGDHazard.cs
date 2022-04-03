abd56e22a4d171920626a59ddca84548dfb200f9
Sam <samuelmcdouall@gmail.com>
2022-04-03 02:20:25 +0100

    Renaming of hazard

--- CGD/Connected Games Development/Assets/Scripts/CGDHazard.cs
+++ CGD/Connected Games Development/Assets/Scripts/CGDHazard.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CGDLavaHazard : MonoBehaviour
+public class CGDHazard : MonoBehaviour
 {
     [SerializeField]
     float _ultPerDecrPerSecond;
@@ -12,7 +12,7 @@ public class CGDLavaHazard : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             //print("player in the lava");
-            other.gameObject.GetComponent<CGDPlayer>().UltimateCharge -= _ultPerDecrPerSecond * Time.fixedDeltaTime;
+            //other.gameObject.GetComponent<CGDPlayer>().UltimateCharge -= _ultPerDecrPerSecond * Time.fixedDeltaTime; todo shouldn't need this but double check with a test
             other.gameObject.GetComponent<CGDPlayer>().ModifyUltimateCharge(-_ultPerDecrPerSecond * Time.fixedDeltaTime);
         }
     }
