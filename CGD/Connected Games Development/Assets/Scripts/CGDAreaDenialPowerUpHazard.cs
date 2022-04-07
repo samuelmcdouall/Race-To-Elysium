a8af5eb93c2e9c78fc1e0029285ec1e3cae5ad59
Sam <samuelmcdouall@gmail.com>
2022-04-07 03:34:57 +0100

    lava + poison burn on being in hazard

--- CGD/Connected Games Development/Assets/Scripts/CGDAreaDenialPowerUpHazard.cs
+++ CGD/Connected Games Development/Assets/Scripts/CGDAreaDenialPowerUpHazard.cs
@@ -11,6 +11,7 @@ public class CGDAreaDenialPowerUpHazard : MonoBehaviour
     float _ultPerDecrPerSecond;
     [SerializeField]
     float _selfImmuneDelay;
+    public HazardType Hazard;
 
     void Update()
     {
@@ -26,12 +27,36 @@ public class CGDAreaDenialPowerUpHazard : MonoBehaviour
         {
             //other.gameObject.GetComponent<CGDPlayer>().UltimateCharge -= _ultPerDecrPerSecond * Time.fixedDeltaTime; todo shouldn't need this but double check with a test
             other.gameObject.GetComponent<CGDPlayer>().ModifyUltimateCharge(-_ultPerDecrPerSecond * Time.fixedDeltaTime);
+            if (!other.gameObject.GetComponent<CGDPlayer>().LavaBurnFX.activeSelf && Hazard == HazardType.LavaPool)
+            {
+                other.gameObject.GetComponent<CGDPlayer>().LavaBurnFX.SetActive(true);
+            }
+            else if (!other.gameObject.GetComponent<CGDPlayer>().PoisonBurnFX.activeSelf && Hazard == HazardType.PoisonCloud)
+            {
+                other.gameObject.GetComponent<CGDPlayer>().PoisonBurnFX.SetActive(true);
+            }
+            else
+            {
+                print("no effects");
+            }
         }
     }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player" && (other.gameObject != OwnPlayer || _selfImmuneDelay <= 0.0f))
         {
+            //if (Hazard == HazardType.LavaPool)
+            //{
+            //    other.gameObject.GetComponent<CGDPlayer>().LavaBurnFX.SetActive(true);
+            //}
+            //else if (Hazard == HazardType.PoisonCloud)
+            //{
+            //    other.gameObject.GetComponent<CGDPlayer>().PoisonBurnFX.SetActive(true);
+            //}
+            //else
+            //{
+            //    print("no effects");
+            //}
             print("player entered the area denial hazard");
         }
     }
@@ -39,7 +64,25 @@ public class CGDAreaDenialPowerUpHazard : MonoBehaviour
     {
         if (other.gameObject.tag == "Player" && (other.gameObject != OwnPlayer || _selfImmuneDelay <= 0.0f))
         {
+            if (Hazard == HazardType.LavaPool)
+            {
+                other.gameObject.GetComponent<CGDPlayer>().LavaBurnFX.SetActive(false);
+            }
+            else if (Hazard == HazardType.PoisonCloud)
+            {
+                other.gameObject.GetComponent<CGDPlayer>().PoisonBurnFX.SetActive(false);
+            }
+            else
+            {
+                print("no effects");
+            }
             print("player exited the area denial hazard");
         }
+    }
+
+    public enum HazardType
+    {
+        LavaPool,
+        PoisonCloud
     }
 }
