bd8a50c924f90a2bb1b7a8bd9a77e6d44693d30c
Sam <samuelmcdouall@gmail.com>
2022-04-12 03:26:39 +0100

    Sorted the issue where this would run for each player rather than just once

--- CGD/Connected Games Development/Assets/Scripts/CGDVictoryPickup.cs
+++ CGD/Connected Games Development/Assets/Scripts/CGDVictoryPickup.cs
@@ -6,14 +6,20 @@ using UnityEngine;
 public class CGDVictoryPickup : MonoBehaviour
 {
     bool _hit = false; // extra precaution todo maybe not needed
+    PhotonView _view;
+
+    private void Start()
+    {
+        _view = GetComponent<PhotonView>();
+    }
     void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.CompareTag("Player") && !_hit)
+        if (collider.gameObject.CompareTag("Player") && !_hit && _view.IsMine)
         {
             _hit = true;
             print("hit the goal, can now destroy object (should only happen once)");
             int winnerPhotonViewID = collider.gameObject.GetComponent<PhotonView>().ViewID;
-            // this is getting called twice for each player (it won't make a difference but it shouldn't be doing so)
+            // todo I think this is sorted, it was getting done x amount of times where x is the number of players, check
             collider.gameObject.GetComponent<CGDPlayer>().DisplayGameOverScreen();
             Destroy(gameObject);
         }
