80858c8b83197eb5f0e67536cf2bd382c8aa903d
Sam <samuelmcdouall@gmail.com>
2022-04-15 18:39:18 +0100

    Update CGDDummyMovingPlayer.cs

--- CGD/Connected Games Development/Assets/Scripts/Unused (delete at end)/CGDDummyMovingPlayer.cs
+++ CGD/Connected Games Development/Assets/Scripts/Unused (delete at end)/CGDDummyMovingPlayer.cs
@@ -9,39 +9,39 @@ public class CGDDummyMovingPlayer : CGDPlayer
     public Transform EndPosition;
     bool _aiStartToEnd;
 
-    void Start()
-    {
-        InitialPlayerSetup();
-    }
+    //void Start()
+    //{
+    //    InitialPlayerSetup();
+    //}
 
-    public override void FixedUpdate()
-    {
-        LimitSpeedToMaximum();
-        if (_aiStartToEnd)
-        {
-            Vector3 DistanceToTarget = Vector3.Normalize(EndPosition.position - transform.position);
-            //PlayerRb.AddForce(DistanceToTarget * PlayerMoveForce * _speedModifier);
-            if (Vector3.Distance(transform.position, EndPosition.position) <= 0.1f)
-            {
-                _aiStartToEnd = false;
-            }
-        }
-        else
-        {
-            Vector3 DistanceToTarget = Vector3.Normalize(StartPosition.position - transform.position);
-            //PlayerRb.AddForce(DistanceToTarget * PlayerMoveForce * _speedModifier);
-            if (Vector3.Distance(transform.position, StartPosition.position) <= 0.1f)
-            {
-                _aiStartToEnd = true;
-            }
-        }
+    //public override void FixedUpdate()
+    //{
+    //    LimitSpeedToMaximum();
+    //    if (_aiStartToEnd)
+    //    {
+    //        Vector3 DistanceToTarget = Vector3.Normalize(EndPosition.position - transform.position);
+    //        //PlayerRb.AddForce(DistanceToTarget * PlayerMoveForce * _speedModifier);
+    //        if (Vector3.Distance(transform.position, EndPosition.position) <= 0.1f)
+    //        {
+    //            _aiStartToEnd = false;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Vector3 DistanceToTarget = Vector3.Normalize(StartPosition.position - transform.position);
+    //        //PlayerRb.AddForce(DistanceToTarget * PlayerMoveForce * _speedModifier);
+    //        if (Vector3.Distance(transform.position, StartPosition.position) <= 0.1f)
+    //        {
+    //            _aiStartToEnd = true;
+    //        }
+    //    }
         
-    }
+    //}
 
-    public override void InitialPlayerSetup()
-    {
-        //PlayerRb = GetComponent<Rigidbody>();
-        _aiStartToEnd = true;
-        //_speedModifier = 1.0f;
-    }
+    //public override void InitialPlayerSetup()
+    //{
+    //    //PlayerRb = GetComponent<Rigidbody>();
+    //    _aiStartToEnd = true;
+    //    //_speedModifier = 1.0f;
+    //}
 }
