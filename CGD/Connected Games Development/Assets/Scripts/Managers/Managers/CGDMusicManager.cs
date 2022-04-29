c8b37b45d6a5615ec7e14b99dc84f248ea6ae12f
Sam <samuelmcdouall@gmail.com>
2022-04-29 03:16:22 +0100

    loop music added

--- CGD/Connected Games Development/Assets/Scripts/Managers/Managers/CGDMusicManager.cs
+++ CGD/Connected Games Development/Assets/Scripts/Managers/Managers/CGDMusicManager.cs
@@ -3,19 +3,34 @@ using UnityEngine.UI;
 
 public class CGDMusicManager : MonoBehaviour
 {
-    public AudioClip Music;
+    public AudioClip IntroMusic;
+    public AudioClip LoopMusic;
     public Slider MusicSlider;
     public Slider SoundSlider;
     AudioSource _as;
+    bool _loopMusicPlaying;
     void Start()
     {
         _as = GetComponent<AudioSource>();
-        _as.loop = true;
-        _as.clip = Music;
+        _loopMusicPlaying = false;
+        _as.clip = IntroMusic;
         _as.volume = CGDGameSettings.MusicVolume;
         _as.Play();
         MusicSlider.value = CGDGameSettings.MusicVolume;
         SoundSlider.value = CGDGameSettings.SoundVolume;
+    }
+
+    void Update()
+    {
+        if (!_loopMusicPlaying && !_as.isPlaying)
+        {
+            _as.Stop();
+            _as.loop = true;
+            _as.clip = LoopMusic;
+            _as.volume = CGDGameSettings.MusicVolume;
+            _as.Play();
+            _loopMusicPlaying = true;
+        }    
     }
 
     public void UpdateMusicVolume(float volume) 
