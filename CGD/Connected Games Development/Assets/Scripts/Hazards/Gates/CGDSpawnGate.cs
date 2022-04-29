bd4fafbfa89e1149accfb1218f3fc9793a45b97b
Sam <samuelmcdouall@gmail.com>
2022-04-29 03:15:41 +0100

    done via transform since going through stuff

--- CGD/Connected Games Development/Assets/Scripts/Hazards/Gates/CGDSpawnGate.cs
+++ CGD/Connected Games Development/Assets/Scripts/Hazards/Gates/CGDSpawnGate.cs
@@ -9,27 +9,21 @@ public class CGDSpawnGate : MonoBehaviour
     float _positionThreshold;
     [System.NonSerialized]
     public bool Moving;
-    Rigidbody _rigidbody;
 
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
         Moving = false;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (Moving)
         {
-            _rigidbody.velocity = new Vector3(0.0f, _speed, 0.0f);
+            transform.position += new Vector3(0.0f, -_speed * Time.deltaTime, 0.0f);
             if (Vector3.Distance(transform.position, EndPosition.position) <= _positionThreshold)
             {
                 Moving = false;
             }
-        }
-        else
-        {
-            _rigidbody.velocity = Vector3.zero;
         }
     }
 }
