4a92cc554fd4411ebefee3e33c3176ff79989492
Sam <samuelmcdouall@gmail.com>
2022-04-30 22:50:18 +0100

    Added tags for ground check to ignore

--- CGD/Connected Games Development/Assets/Scripts/Player/General/CGDGroundCheck.cs
+++ CGD/Connected Games Development/Assets/Scripts/Player/General/CGDGroundCheck.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CGDGroundCheck : MonoBehaviour
 {
     public bool IsGrounded;
+    List<string> _ignoredTags = new List<string>{"Cloud", "CharacterSelect", "AreaDenialHazard", "UltPickup", "ArachneWeb"};
 
     void OnTriggerStay(Collider collider)
     {
-        if (collider != null && collider.gameObject.tag != "Cloud")
+        if (collider != null && !_ignoredTags.Contains(collider.gameObject.tag))
         {
             IsGrounded = true;
         }
